5949fd1fabcdc636886ae888f6e970bdde3505b2
suhanyujie <suhanyujie@qq.com>
2021-04-12 23:47:46 +0800

    add some http code

--- libs/framwork/base3/core/engine/context.go
+++ libs/framwork/base3/core/engine/context.go
@@ -1,7 +1,46 @@
 package engine
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Context struct {
-	Writer http.ResponseWriter
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
 }
